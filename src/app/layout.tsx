16b2da13824431890f1f22de4e5ce2482cf60bd0
Valdo-117 <keyerdelahozsteam@gmail.com>
2024-12-02 23:53:11 -0500

    add: img seo and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,36 @@ import Nav from "@/components/layout/Nav";
 const rajdhani = Rajdhani({ subsets: ['latin'], weight: ["300","400","500","600","700"] })
 
 export const metadata: Metadata = {
-  title: "Alfa - Medición de Temperatura para Paneles Solares",
+  title: "Alfa | Medidores de Temperatura para Paneles Solares",
   description:
-    "Alfa es líder en la fabricación de medidores de temperatura para paneles solares. Descubre nuestros productos diseñados para soportar condiciones extremas.",
+    "Descubre Alfa, líderes en la fabricación de medidores de temperatura de alta resistencia para paneles solares. Soluciones innovadoras y duraderas diseñadas para soportar condiciones extremas.",
+  openGraph: {
+    title: "Alfa | Medidores de Temperatura para Paneles Solares",
+    description:
+      "Bienvenido a Alfa. Conoce nuestros medidores de temperatura diseñados para paneles solares, fabricados para resistir agua, tierra, polvo, caídas y más. Energía solar sostenible con calidad garantizada.",
+    type: "website",
+    images: {
+      url: "https://firebasestorage.googleapis.com/v0/b/unidotaciones-f49fc.appspot.com/o/Preview_alpa.png?alt=media&token=87727b38-b234-4a27-b770-21cbeb970698",
+      width: 1200,
+      height: 630,
+      alt: "Vista previa de los medidores de temperatura de Alfa",
+    },
+    siteName: "Alfa | Energía Solar",
+    locale: "es_ES",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Alfa | Medidores de Temperatura para Paneles Solares",
+    description:
+      "Explora los medidores de temperatura de Alfa, diseñados para soportar las condiciones más extremas en energía solar. Calidad y confiabilidad garantizadas.",
+    images: [
+      "https://firebasestorage.googleapis.com/v0/b/unidotaciones-f49fc.appspot.com/o/Preview_alpa.png?alt=media&token=87727b38-b234-4a27-b770-21cbeb970698",
+    ],
+  },
 };
+
+
+
 
 export default function RootLayout({
   children,
